d27df26c9f74ae11bbec00bc0ce9e5eec6be52d9
SuhasBharadwajK <suhas.five@gmail.com>
2014-08-09 03:08:20 +0530

    Revert "First Phase of Success"
    This reverts commit 70b95e975a8e1dc0e4994cc6cddc0c29c0726da0.

--- app/src/main/java/app/android/ball/ball/SecondScreen.java
+++ app/src/main/java/app/android/ball/ball/SecondScreen.java
@@ -23,7 +23,7 @@ import android.app.Activity;
     public class SecondScreen extends View {
         Paint myPaint = new Paint();
         int dirx = 1, diry = 1;
-        int x, y,st, flag, flag2, factX = 10, factY = 10, a, b;
+        int x, y,st, flag, flag2;
         /*DisplayMetrics metrics = getResources().getDisplayMetrics();
         int x = metrics.heightPixels;
         int y = metrics.widthPixels;*/
@@ -32,38 +32,33 @@ import android.app.Activity;
         }
         public void onDraw(Canvas can){
             if(st==0) {
-                x = a = can.getWidth() / 2;
-                y = b = can.getHeight() / 2;
+                x = can.getWidth() / 2;
+                y = can.getHeight() / 2;
                 st = 1;
-            }
-            if(a == x && b == y) {
-                factX = 10;
-                factY = 10;
             }
             myPaint.setColor(Color.parseColor("#0099cc"));
             can.drawCircle(x,y,40,myPaint);
-            if(flag == 1 /*&& flag2%2 == 0*/) {
+            if(flag == 1 && flag2%2 == 0) {
                 if (x < can.getWidth() && dirx == 1) {
-                    x += factX;
-                    if (x > can.getWidth() - 1) {
+                    x += 10;
+                    if (x > can.getWidth() - 5)
                         dirx = 2;
-                    }
                 }
                 if (x > 0 && dirx == 2) {
-                    x -= factX;
+                    x -= 10;
                     if (x < 3)
                         dirx = 1;
                 }
                 if (y < can.getHeight() && diry == 1) {
-                    y += factY;
-                    if (y > can.getHeight() - 1) {
+                    y += 10;
+                    if (y > can.getHeight() - 20) {
                         diry = 2;
                         //Toast.makeText(getContext(),"y:"+ y, Toast.LENGTH_LONG).show();
 
                     }
                 }
                 if (y > 0 && diry == 2) {
-                    y -= factY;
+                    y -= 10;
                     if (y < 3)
                         diry = 1;
                 }
@@ -73,36 +68,10 @@ import android.app.Activity;
         public boolean onTouchEvent( MotionEvent e ) {
             int action = e.getAction();
             if(action == MotionEvent.ACTION_DOWN){
-                a = (int) e.getX();
-                b = (int) e.getY();
                 flag = 1;
                 flag2++;
                 if(flag2 == 15)
                     flag2 = 1;
-                if(x < a && y > b) {
-                    dirx = 1;
-                    diry = 2;
-                    factX = a - x /*- *//*(a-x/50)*/;
-                    factY = y - b /*- *//*(y-b/50)*/;
-                }
-                if(x > a && y > b) {
-                    dirx = 2;
-                    diry = 2;
-                    factX = x - a /*- *//*(x-a/50)*/;
-                    factY = y - b /*- *//*(y-b/50)*/;
-                }
-                if(x > a && y < b) {
-                    dirx = 2;
-                    diry = 1;
-                    factX = x - a /*- *//*(x-a/50)*/;
-                    factY = b - y/*- *//*(b-y/50)*/;
-                }
-                if(x < a && y < b) {
-                    dirx = 1;
-                    diry = 1;
-                    factX = a - x /*- *//*(a-x/50)*/;
-                    factY = b - y /*- *//*(b-y/50)*/;
-                }
             }
             return true;
         }
